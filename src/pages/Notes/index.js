459b8c5d8a51fd787755f3dd49a316020c9c7bfb
B6020712 <boss123440@hotmail.com>
2023-06-28 18:10:59 +0700

    1:09:03

---
+++ src/pages/Notes/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View } from "react-native";
+
+function Notes() {
+    return (
+        <View></View>
+    )
+}
+
+export default Notes;
---
+++ src/styles/colors.js
@@ -0,0 +1,12 @@
+const COLORS = {
+    homeBackground: "#12AB12",
+    noteBackground: "12AA44",
+    notes         : "AB12AA",
+    header        : "E12FFF",
+    headerButtons : "FFFFFF",
+    addButton     : "120AAA",
+    loading       : "BBB12B",
+    searchBar     : "12EEE0"
+}
+
+export default COLORS;
