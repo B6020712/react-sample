c3c069e2152a5b30fa844f16cf63840dc2d350f0
B6020712 <boss123440@hotmail.com>
2023-07-02 23:34:27 +0700

    complete checkbox and add sidebar

--- src/screens/main-screen.tsx
+++ src/screens/main-screen.tsx
@@ -2,7 +2,7 @@ import React, { useCallback, useState } from 'react'
 import { Pressable } from 'react-native'
 import { Text, Box, Center, VStack, useColorModeValue } from 'native-base'
 import ThemeToggle from '../components/theme-toggle'
-import AnimatedCheckbox from '../components/animated-checkbox'
+import TaskItem from '../components/task-item'
 
 export default function MainScreen() {
     const [ checked, setChecked ] = useState(false)
@@ -12,11 +12,7 @@ export default function MainScreen() {
     return (
         <Center _dark={{bg: 'blueGray.900'}} _light={{bg: 'blueGray.50'}} px={4} flex={1}>
             <VStack space={5} alignItems="center">
-                <Box w="100px" h="100px">
-                    <Pressable onPress={handlePressCheckbox}>
-                        <AnimatedCheckbox checked={checked} />
-                    </Pressable>
-                </Box>
+                <TaskItem isDone={checked} onToggleCheckbox={handlePressCheckbox} />
                 <Box p={10} bg={useColorModeValue( 'red.500', 'yellow.500' )}>
                     <Text>Hello</Text>
                 </Box>
